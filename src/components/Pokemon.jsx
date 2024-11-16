58762ffa1d0a30ecf443f9a309ac5435174f9cd9
Kinga <kinga.maleszewska@o2.pl>
2024-11-16 11:29:01 +0100

    Improve Pokemon app

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,18 +3,23 @@ import axios from 'axios';
 
 const Pokemon = () => {
     const [num, setNum] = useState();
+    const [name, setName] = useState();
+    const [moves, setMoves] = useState();
 
     useEffect(() => {
         async function getData() {
             const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-            console.log(res.data.name);
+            setName(res.data.name);
+            console.log(res.data.moves)
+            setMoves(res.data.moves.length);
         }
         getData();
     })
     return (
         <>
-            <h1>You choose {num} value</h1>
-            <h1>My name is { }</h1>
+            <h1>You choose <span style={{ color: 'gray' }}>{num}</span> value</h1>
+            <h1>My name is <span style={{ color: 'gray' }}>{name}</span></h1>
+            <h1>I have <span style={{ color: 'gray' }}>{moves}</span> moves</h1>
             <select name="" id="" value={num} onChange={(event) => {
                 setNum(event.target.value);
             }}>
